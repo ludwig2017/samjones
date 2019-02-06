b7d51c9f59bc002eb01397ac0bca2f26159eb2ce
ludwig2017 <napigkitludwigbethnicer@gmail.com>
2019-02-06 20:51:24 +0800

    update

--- app/Controllers/User/UserController.php
+++ app/Controllers/User/UserController.php
@@ -11,8 +11,31 @@
 		public $user_name;
 		public $user_password;
 		public $user_email;
-		public $;
-		public $;
-		public $;
-		public $;
+		public $displayname;
+		public $ustat;
+		public $acctlevel;
+		public $datecreated;
+
+		// Constractor with DB
+		public function __constractor($db) {
+			$this->dbcnn = $db;
+		}
+
+		// Get POSTs
+		public function read() {
+
+			// Create Query
+			$query = 'SELECT 
+				c.username as username_name, 
+				p.user_id, 
+				p.user_password, 
+				p.user_email, 
+				p.displayname, 
+				p.ustat, 
+				p.acctlevel, 
+				p.datecreated 
+			FROM
+				' . $this- . '
+			'
+		}
 	}
