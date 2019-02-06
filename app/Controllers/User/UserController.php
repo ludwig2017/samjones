cd12b99ba357ad85b3fa214abb649298e6a0413a
ludwig2017 <napigkitludwigbethnicer@gmail.com>
2019-02-06 14:02:06 +0800

    api

--- app/Controllers/User/UserController.php
+++ app/Controllers/User/UserController.php
@@ -1,3 +1,18 @@
 <?php
 
-	class User
+	class UserController {
+
+		// Database
+		private $dbcnn;
+		private $tblusers = 'posts';
+
+		// POST Properties
+		public $user_id;
+		public $user_name;
+		public $user_password;
+		public $user_email;
+		public $;
+		public $;
+		public $;
+		public $;
+	}
--- config/database.php
+++ config/database.php
@@ -14,7 +14,7 @@
 			$this->dbcnn = null;
 
 			try {
-				$this->dbcnn = new PDO("mysql:dbname=".$this->dbname.";host=".$this->shost."", "".$this->username."", "".$this->password."");
+				$this->dbcnn = new PDO("mysql:dbname=".$this->dbname.";host=".$this->shost, $this->username, $this->password);
 				$this->dbcnn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			} catch(PDOException $exception) {
 				echo "Database Connection error: " . $exception->getMessage();
